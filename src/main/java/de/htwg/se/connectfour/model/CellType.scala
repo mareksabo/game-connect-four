f9abc217c8d8bbaa6ab40d5a7b6135f3263cd280
Marek Šabo <xsabo4@fi.muni.cz>
2017-04-05 17:54:27 +0200

    Model package has 100% code coverage

--- src/main/java/de/htwg/se/connectfour/model/CellType.scala
+++ src/main/java/de/htwg/se/connectfour/model/CellType.scala
@@ -1,8 +1,5 @@
 package de.htwg.se.connectfour.model
 
-/**
-  * Created by mrek on 24.3.2017.
-  */
 object CellType extends Enumeration {
   val Empty, X, O = Value
 }
---
+++ src/test/java/de/htwg/se/connectfour/model/PlayerTest.scala
@@ -0,0 +1,23 @@
+package de.htwg.se.connectfour.model
+
+import java.io.ByteArrayInputStream
+
+import org.specs2.mutable.Specification
+
+class PlayerTest extends Specification {
+
+  "A new player Ralf" should {
+
+    val player = new Player("Ralf", CellType.O)
+
+    "method parses a number which player writes" in {
+      val number = 5
+      val input = number + "\n"
+      System.setIn(new ByteArrayInputStream(input.getBytes))
+
+      val readInput = player.getInput
+      number must be_==(readInput)
+    }
+
+  }
+}

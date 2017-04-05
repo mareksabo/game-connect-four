ce8a39ba6fa6a2f40b2ccf33efd3c598be7751ba
Marek Šabo <xsabo4@fi.muni.cz>
2017-04-05 19:42:24 +0200

    Constant name changed and another one added

--- src/main/java/de/htwg/se/connectfour/logic/CheckWinner.scala
+++ src/main/java/de/htwg/se/connectfour/logic/CheckWinner.scala
@@ -2,11 +2,9 @@ package de.htwg.se.connectfour.logic
 
 import de.htwg.se.connectfour.model.{Cell, Grid}
 
-/**
-  * Created by Kuba on 05.04.2017.
-  */
 class CheckWinner {
-  val ADJOININGCELLS: Int = 3
+  val NUMBER_OF_CELLS_TO_WIN = 4
+  val CELLS_AROUND_TO_WIN: Int = NUMBER_OF_CELLS_TO_WIN - 1
 
   def checkForWinner(grid: Grid, cell: Cell): Boolean =
     checkVertical(grid, cell) || checkHorizontal(grid, cell) || checkDiagonal(grid, cell)
@@ -14,24 +12,24 @@ class CheckWinner {
 
   def checkVertical(grid: Grid, cell: Cell): Boolean = {
     var winCounter = 0
-    for (i <- cell.y to cell.y + ADJOININGCELLS) {
-      if (grid.isCellValid(cell.x, cell.y + ADJOININGCELLS) && grid.cell(cell.x, i).cellType == cell.cellType)
+    for (i <- cell.y to cell.y + CELLS_AROUND_TO_WIN) {
+      if (grid.isCellValid(cell.x, cell.y + CELLS_AROUND_TO_WIN) && grid.cell(cell.x, i).cellType == cell.cellType)
         winCounter += 1
       else
         winCounter = 0
     }
-    winCounter == 4
+    winCounter == NUMBER_OF_CELLS_TO_WIN
   }
 
   def checkHorizontal(grid: Grid, cell: Cell): Boolean = {
     var winCounter = 0
-    for (i <- cell.x - ADJOININGCELLS to cell.x + ADJOININGCELLS) {
+    for (i <- cell.x - CELLS_AROUND_TO_WIN to cell.x + CELLS_AROUND_TO_WIN) {
       if (grid.isCellValid(i, cell.y) && (grid.cell(i, cell.y).cellType == cell.cellType))
         winCounter += 1
-      else if (winCounter != 4)
+      else if (winCounter != NUMBER_OF_CELLS_TO_WIN)
         winCounter = 0
     }
-    winCounter == 4
+    winCounter == NUMBER_OF_CELLS_TO_WIN
   }
 
   def checkDiagonal(grid: Grid, cell: Cell): Boolean = {
@@ -40,28 +38,28 @@ class CheckWinner {
 
   def leftDownToRightUp(grid: Grid, cell: Cell): Boolean = {
     var winCounter = 0
-    for (i <- -ADJOININGCELLS to ADJOININGCELLS) {
+    for (i <- -CELLS_AROUND_TO_WIN to CELLS_AROUND_TO_WIN) {
       {
         if (grid.isCellValid(cell.x - i, cell.y + i) && grid.cell(cell.x - i, cell.y + i).cellType == cell.cellType)
           winCounter += 1
-        else if (winCounter != 4)
+        else if (winCounter != NUMBER_OF_CELLS_TO_WIN)
           winCounter = 0
       }
     }
-    winCounter == 4
+    winCounter == NUMBER_OF_CELLS_TO_WIN
   }
 
   def leftUpToRightDown(grid: Grid, cell: Cell): Boolean = {
     var winCounter = 0
-    for (i <- -ADJOININGCELLS to ADJOININGCELLS) {
+    for (i <- -CELLS_AROUND_TO_WIN to CELLS_AROUND_TO_WIN) {
       {
         if (grid.isCellValid(cell.x + i, cell.y + i) && grid.cell(cell.x + i, cell.y + i).cellType == cell.cellType) {
           winCounter += 1
-        } else if (winCounter != 4)
+        } else if (winCounter != NUMBER_OF_CELLS_TO_WIN)
           winCounter = 0
       }
     }
-    winCounter == 4
+    winCounter == NUMBER_OF_CELLS_TO_WIN
   }
 
 }

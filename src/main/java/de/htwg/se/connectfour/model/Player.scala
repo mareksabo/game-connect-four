0ee76d5448f6b4288ad139ef150814e669895c7e
Marek Sabo <xsabo4@fi.muni.cz>
2017-04-28 22:04:30 +0200

    Pom removed

--- src/main/java/de/htwg/se/connectfour/model/Player.scala
+++ src/main/java/de/htwg/se/connectfour/model/Player.scala
@@ -1,10 +1,12 @@
 package de.htwg.se.connectfour.model
 
+import scala.io.StdIn
+
 class Player(val name: String, val _cellType: CellType.Value) {
 
   def readInput(): Int = {
     print("Player " + name + ", write column number: ")
-    Console.readInt()
+    StdIn.readInt()
   }
 
 }

1ed9c0d43846d6ddd7fa5674bbc6a75c3d7d3926
Kuba1993 <David.Kubatzki@htwg-konstanz.de>
2017-06-13 18:49:03 +0200

    started implementing tests for Game PrintGame and DumbBotPlayer

--- src/test/java/de/htwg/se/connectfour/logic/GameTest.scala
+++ src/test/java/de/htwg/se/connectfour/logic/GameTest.scala
@@ -1,8 +1,23 @@
 package de.htwg.se.connectfour.logic
 
-/**
-  * Created by Kuba on 10.06.2017.
-  */
-class GameTest {
+import de.htwg.se.connectfour.mvc.controller.GridController
+import de.htwg.se.connectfour.mvc.model.Grid
+import de.htwg.se.connectfour.mvc.model.player.{ GamingPlayers, RealPlayer }
+import org.specs2.mutable.Specification
 
+class GameTest extends Specification {
+
+  "New GridController of grid 3x2" should {
+    val player1 = RealPlayer("player1")
+    val player2 = RealPlayer("player2")
+    val grid = new Grid
+    val gridController = new GridController(grid)
+    val players = GamingPlayers(player1, player2)
+    val game = new Game(gridController, players)
+    "have initialized output" in {
+      game.gridController must be_==(gridController)
+    }
+
+
+  }
 }
